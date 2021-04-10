161c05ac0c1a2294ea87a9e7836cc510945eb147
= <=>
2021-04-09 22:03:00 -0400

    Add user stuff

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 var db = mysql.createConnection({
   host: 'localhost',
@@ -9,12 +11,68 @@ var db = mysql.createConnection({
   database: 'ReciPlease',
 });
 
-app.get('/', (require, response) => {
-  const sqlInsert = "INSERT INTO '' ('', '') VALUES ('', '')";
-  db.query(sqlInsert, (err, result) => {
-    response.send("Hello world!!!");
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// users endpoints
+app.get("/api/users/get", (require, response) => {
+  const sqlSelect = "SELECT * FROM users";
+  db.query(sqlSelect, (err, result) => {
+      response.send(result);
+  });
+});
+
+app.get("/api/users/getSelected/:firstName", (require, response) => {
+  const firstName = require.params.firstName;
+
+  const sqlSelect = "SELECT * FROM users WHERE `firstName`= ?";
+  db.query(sqlSelect, firstName, (err, result) => {
+      response.send(result);
+
+      if (err) 
+      console.log(error);
+  });
+});
+
+app.post("/api/users/insert", (require, response) => {
+  const firstName = require.body.firstName;
+  const lastName = require.body.lastName;
+  const email = require.body.email;
+
+  const sqlInsert = "INSERT INTO `users` (`first_name`, `last_name`, `email`) VALUES (?,?,?)";
+  db.query(sqlInsert, [firstName, lastName, email], (err, result) => {
+      console.log(error);
   })
 });
+
+app.delete("/api/users/delete/:email", (require, response) => {
+  const email = require.params.email;
+
+  const sqlDelete = "DELETE FROM `users` WHERE `email`= ?";
+  db.query(sqlDelete, email, (err, result) => {
+      if (err) 
+      console.log(error);
+  })
+});
+
+app.put("/api/users/update/", (require, response) => {
+  const firstName = require.body.firstName;
+  const lastName = require.body.lastName;
+  const email = require.body.email;
+
+  const sqlUpdate = "UPDATE `users` SET `firstName` = ?, `lastName` = ? WHERE `email`= ?";
+  db.query(sqlUpdate, [firstName, lastName, email], (err, result) => {
+      if (err) 
+      console.log(error);
+  })
+});
+
+// recipes endpoints
+
+// ingredients endpoints
+
+// tags endpoints
 
 app.listen(3002, () => {
   console.log("running on port 3002");
---
+++ reciplease/src/Users.js
@@ -0,0 +1,115 @@
+import './App.css';
+import React, {useState, useEffect} from "react";
+import Axios from 'axios';
+
+function App() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [userList, setUsersList] = useState([]);
+
+    const [newFirstName, setNewFirstName] = useState("");
+    const [newLastName, setNewLastName] = useState("");
+
+    const [searchFirstName, setSearchFirstName] = useState("");
+
+    useEffect(() => {
+      Axios.get('http://localhost:3002/api/users/get').then((response) => {
+        setUsersList(response.data)
+      })
+    },[]);
+  
+    const submitUser = () => { 
+      Axios.post('http://localhost:3002/api/users/insert', {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+      });
+      
+      setUsersList([
+        ...userList,
+        {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+        },
+      ]);
+    };
+  
+    const deleteUser = (email) => {
+      Axios.delete(`http://localhost:3002/api/users/delete/${email}`);
+    };
+  
+    const updateUser = (email) => {
+        Axios.put(`http://localhost:3002/api/users/update`, {
+          firstName: newFirstName,
+          lastName: newLastName,
+          email: email,
+        });
+        setNewFirstName(""),
+        setNewLastName("")
+    };
+
+    const searchUser = () => {
+        Axios.get(`http://localhost:3002/api/users/getSelected/${searchFirstName}`).then((response) => {
+        setUsersList(response.data)
+      })
+    },[];
+  
+    return (
+      <div className="App">
+        <h1> Users</h1>
+  
+        <div className="form">
+          <label> First Name:</label>
+          <input type="text" name="first_name" onChange={(e) => {
+            setFirstName(e.target.value)
+          } }/>
+          <label> Last Name:</label>
+          <input type="text" name="last_name" onChange={(e) => {
+            setLastName(e.target.value)
+          } }/>
+          <label> Email:</label>
+          <input name="email" onChange={(e) => {
+            setEmail(e.target.value)
+          }}/>
+          
+          <button onClick={submitUser}> Submit</button>
+  
+          <input name="searchFirstName" onChange={(e) => {
+              setSearchFirstName(e.target.value)
+          }}></input>
+
+          <button onClick={() => {
+              searchUser()
+          }}> Populate</button>
+
+          {userList.map((val) => {
+            return (
+              <div className = "card">
+                <h1> Name: {val.firstName} {val.lastName} </h1>
+                <p> Email: {val.email}</p>
+                <button onClick={() => { deleteUser(val.email) }}> Delete</button>
+                <input type="text" id="updateFirstName" onChange={(e) => {
+                  setNewFirstName(e.target.value)
+                } }/>
+                <input type="text" id="updateLastName" onChange={(e) => {
+                  setNewLastName(e.target.value)
+                } }/>
+                <button onClick={() => {
+                  updateUser(val.email)
+                }}> Update</button>
+                </div>
+            );
+            
+            ;
+          })}
+        
+        </div>
+        
+      </div>
+    );
+  }
+  
+  export default App;
+  
