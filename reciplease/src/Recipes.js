79f7a6da48d822790e233cf26fce8d3dd76cde72
= <=>
2021-04-10 05:11:55 -0400

    Add useStates and useEffect

--- reciplease/src/Recipes.js
+++ reciplease/src/Recipes.js
@@ -2,6 +2,28 @@ import './App.css';
 import React, {useState, useEffect} from "react";
 import Axios from 'axios';
 
+const [rating, set_rating] = useState('');
+const [prep_time_minutes, set_prep_time_minutes] = useState('');
+const [serving_size, set_serving_size] = useState('');
+const [recipe_description, set_recipe_description] = useState('');
+const [recipe_name, set_recipe_name] = useState('');
+
+const [recipeList, setRecipesList] = useState([]);
+
+const [new_rating, setNewrating] = useState("");
+const [new_prep_time_minutes, setNewprep_time_minutes] = useState("");
+const [new_serving_size, setNewserving_size] = useState("");
+const [new_recipe_description, setNewrecipe_description] = useState("");
+const [new_recipe_name, setNewrecipe_name] = useState("");
+
+const [searchrecipe_name, setSearchrecipe_name] = useState("");
+
+useEffect(() => {
+  Axios.get('http://localhost:3002/api/recipes/get').then((response) => {
+    setRecipesList(response.data)
+  })
+})
+
 function Recipes() {
   return (
     <div className="Recipes">
